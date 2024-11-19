af54b7642b2013f82eb33dc2d6fac310afd35a9d
MaxWix <mw3397@drexel.edu>
2024-11-19 09:56:35 -0500

    updates

---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavMenu from "../components/navigation";
+
+const Home = () => {
+  console.log("Home");
+  return (
+    <div className="TourReview">
+      <h1>Home</h1>
+      <NavMenu />
+    </div>
+  );
+};
+
+export default Home;
