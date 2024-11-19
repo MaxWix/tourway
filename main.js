52ac078fe1b714b94d78341718a63aa3a5fb17cd
MaxWix <mw3397@drexel.edu>
2024-11-18 19:09:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import App from "./app.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
