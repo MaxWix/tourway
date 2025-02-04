0d315542c7dd84fdb5ba91a00fc67f993c8d4a4c
MaxWix <mw3397@drexel.edu>
2025-02-03 21:53:44 -0500

    Update universityBio.js
    add questionarre link

--- src/pages/universityBio.js
+++ src/pages/universityBio.js
@@ -1,5 +1,6 @@
 import React from "react";
-import '../styles/styles.global.scss';
+import { useNavigate } from "react-router-dom";
+import "../styles/styles.global.scss";
 
 import NavMenu from "../components/navigation/NavMenu";
 import Header from "../components/navigation/Header";
@@ -7,20 +8,28 @@ import Header from "../components/navigation/Header";
 import DrexelOverview from "../assets/imgs/drexel-overview.jpg";
 
 const UniversityBio = () => {
+  const navigate = useNavigate();
+
+  const goToQuestionnare = () => {
+    navigate("/tour/questionnaire");
+  };
+
   console.log("UniversityBio");
   return (
     <div className="universityBio">
-      <Header HeaderIMG={DrexelOverview}
-              headerTitle="Drexel University"
-              headerSubtitle="Philadelphia, PA"
-              showBackButton={true}
-              showHeartButton={true}
-              isHearted={false}
-               />
-    <div className="MainContent">
-      <h1>University Bio</h1>
-      <NavMenu />
-    </div>
+      <Header
+        HeaderIMG={DrexelOverview}
+        headerTitle="Drexel University"
+        headerSubtitle="Philadelphia, PA"
+        showBackButton={true}
+        showHeartButton={true}
+        isHearted={false}
+      />
+      <div className="MainContent">
+        <h1>University Bio</h1>
+        <NavMenu />
+        <button onClick={goToQuestionnare}>go to questionnare</button>
+      </div>
     </div>
   );
 };
