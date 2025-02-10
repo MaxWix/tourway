c662ef8e3e70b0890e56d85c65906adc8f63d337
mi-chelle-le <ml3725@drexel.edu>
2025-02-10 15:04:41 -0500

    More components & style fixes

--- src/components/universities/InProgressCard/index.js
+++ src/components/universities/InProgressCard/index.js
@@ -1,47 +1,59 @@
 import React from "react";
 import styles from './styles.module.scss';
 import CircleButton from "../../common/CircleButton";
+import Button from "../../common/Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong, faXmark, faPen } from "@fortawesome/free-solid-svg-icons";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+
+import DrexelLogo from "../../../assets/imgs/drexel-logo.png";
+import LocationIconOutline from "../../../assets/icons/location-outline.svg";
 
 const InProgressCard = () => {
     return (
     <div className={styles.progressCard}>
-      <div className={styles.cardHeader}>
-        <img/>
+      <div className={styles.progressCardHeader}>
+        <div className={styles.progressCardHeaderUni}>
+        <img src={DrexelLogo}/>
         <div className={styles.universityTitle}>
             <h4>Drexel University</h4>
             <p>Ongoing tour</p>
         </div>
+        </div>
         <CircleButton 
-        icon={faMagnifyingGlass} /* edit icon */
+        icon={<FontAwesomeIcon icon={faXmark} />}
         onClick={() => alert("")} 
         bgColor="#0BA3A8" 
         iconColor="#fff" 
       />
       </div>
       <div className={styles.timeStops}>
-        <div>
-            <svg></svg>
-            <p>40 mins left</p>
+        <div className={styles.time}>
+            <FontAwesomeIcon icon={faClock} />
+            <p><span>40</span> mins left</p>
         </div>
-        <div>
-            <svg></svg>
-            <p>6/10 stops left</p>
+        <div className={styles.stops}>
+            <img src={LocationIconOutline} />
+            <p><span>6/10</span> stops</p>
         </div>
-      </div>
+    </div>
       <div className={styles.stopsRemaining}>
-        <p>Stops Remaining</p>
+        <h5>Stops Remaining</h5>
         <div>
-        <p>Mario Statue --- URBN Center --- Urban Eatery --- Bentley --- LeBow Business School</p>
-        <span>+1 stop</span>
+        <p>Mario Statue --- URBN Center --- Urban Eatery --- Bentley --- LeBow Business School<span> +1 stop</span></p>
         </div>
       </div>
-      <div className={styles.CTA}>
+      <div className={styles.progressCardCTA}>
       <CircleButton 
-        icon={faMagnifyingGlass} 
+        icon={<FontAwesomeIcon icon={faPen} />} 
         onClick={() => alert("")} 
         bgColor="#0BA3A8" 
         iconColor="#fff" 
       /> 
+      <Button
+      text="CONTINUE"
+      icon={<FontAwesomeIcon icon={faArrowRightLong} />}
+      height="46px"  /> 
       </div>
       </div>
     );
---
+++ src/pages/testPage.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import Navbar from "../components/navigation/Navbar";
+import InProgressCard from "../components/universities/InProgressCard";
+import StopsProgress from "../components/tour/StopsProgress";
+import CategoryTag from "../components/common/CategoryTag";
+import Counselor from '../components/nextsteps/Counselor';
+
+const TestPage = () => {
+    const handleTagStateChange = (newState) => {
+        console.log("Tag state changed to:", newState);
+      };
+
+    console.log("TestPage");
+    return (
+      <div className="TestPage">
+        <h1>Test Page</h1>
+        <br></br>
+        <div>
+            <h4>In progress card</h4>
+            <InProgressCard />
+            <h4>Stops progress</h4>
+            <StopsProgress
+            stops="1/6" />
+                    <br></br>
+            <h4>Tag with Toggle Ability</h4>
+            <CategoryTag 
+                label="Toggle Tag"
+                defaultSelected={false}
+                selectable={true}
+                onClick={handleTagStateChange}
+            />
+            <br></br>
+            <br></br>
+            <h4>Static Tag (Non-Selectable)</h4>
+            <CategoryTag 
+                label="Static Tag"
+                defaultSelected={true}
+                selectable={false}
+            />
+            <br></br>
+            <br></br>
+            <h4>Counselor card</h4>
+            <Counselor />
+        </div>
+
+        <div>
+        <Navbar />
+        </div>
+      </div>
+    );
+  };
+  
+  export default TestPage;
